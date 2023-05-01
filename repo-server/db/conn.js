bcc29480c74e10df4154e33eb826fb4e0918d0a6
MinhLeeBtasekee <leminh.nguyen@btaskee.com>
2023-05-01 13:38:37 +0700

    feat: connect into free cluster mongodb

--- repo-server/db/conn.js
+++ repo-server/db/conn.js
@@ -1,6 +1,6 @@
 const MongoClient = require("mongodb").MongoClient;
 
-const url = 'mongodb://127.0.0.1:27017'
+const url = process.env.CLUSTER_URL || 'mongodb://127.0.0.1:27017'
 
 const connecting = MongoClient.connect(url, { useNewUrlParser: true }).then((client) => {
   return client.db('3m')
