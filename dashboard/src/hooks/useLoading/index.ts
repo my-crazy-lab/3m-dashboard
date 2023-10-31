82ca30ae799b44462e34d5e147a5ef3bc3ee9b15
MinhLeeBtaskee <leminh.nguyen@btaskee.com>
2023-10-31 18:22:03 +0700

    feat: use new server to call api

--- dashboard/src/hooks/useLoading/index.ts
+++ dashboard/src/hooks/useLoading/index.ts
@@ -1,6 +1,6 @@
-//@ts-nocheck
-import axios from "axios";
+// @ts-nocheck
 import { useState } from "react";
+import axios from "../../axios";
 
 const useLoading = ({ callbackQuery, method, api }: any) => {
   const [isLoading, setIsLoading] = useState<boolean>(false);
@@ -11,7 +11,7 @@ const useLoading = ({ callbackQuery, method, api }: any) => {
 
     await axios[method](api, ...params)
       .then((result) => {
-        setIsLoading(false);
+        setIsLoading(false);  
 
         callbackQuery?.(result, ...params);
 
